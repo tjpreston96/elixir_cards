8431c9bb6bb2731b92f6bec5b598f3235287a311
tpreston96 <tpreston96@gmail.com>
2021-09-07 19:05:32 -0500

    setup

---
+++ lib/cards.ex
@@ -0,0 +1,19 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for `Cards`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cards.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
+#
