da9f682b5cbccbd51261d715d38e4a93cb57efca
tpreston96 <tpreston96@gmail.com>
2021-09-10 00:09:45 -0500

    comprehension pt 2

--- lib/cards.ex
+++ lib/cards.ex
@@ -3,8 +3,8 @@ defmodule Cards do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = [ "Spades", "Clubs", "Hearts", "Diamonds"]
 
-    for suit <- suits do
-      suit
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
     end
   end
 
