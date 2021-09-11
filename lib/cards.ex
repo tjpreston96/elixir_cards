3b60a1120bbb36b97957ce4a1b9ed040c9a5e667
tpreston96 <tpreston96@gmail.com>
2021-09-10 20:24:14 -0500

    documentation and testing

--- lib/cards.ex
+++ lib/cards.ex
@@ -18,6 +18,15 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Determines whether a deck contains a given cards
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
